aea6b92ea3f0a657d18fd8f26cec371e53db424d
PTPrashant <86474829+PTPrashant@users.noreply.github.com>
2021-12-28 14:49:12 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,44 +1,44 @@
 
 document.addEventListener('keypress', function( keyPressed){
-  if( keyPressed.key == 'w' ){
+  if( keyPressed.key == 'w' || keyPressed.key == 'W' ){
 
-      var audio=  new Audio('sounds/tom-1.mp3');
+      var audio=  new Audio('tom-1.mp3');
       audio.play();
   }
 
-  if (keyPressed.key == 'a') {
-    var audio=  new Audio('sounds/tom-2.mp3');
+  if (keyPressed.key == 'a'  || keyPressed.key == 'A') {
+    var audio=  new Audio('tom-2.mp3');
     audio.play();
 
   }
 
-  if (keyPressed.key == 's') {
-    var audio=  new Audio('sounds/tom-3.mp3');
+  if (keyPressed.key == 's'  || keyPressed.key == 'S') {
+    var audio=  new Audio('tom-3.mp3');
     audio.play();
 
   }
 
-  if (keyPressed.key== 'd') {
-    var audio=  new Audio('sounds/tom-4.mp3');
+  if (keyPressed.key== 'd' || keyPressed.key == 'D') {
+    var audio=  new Audio('tom-4.mp3');
     audio.play();
 
   }
 
-  if( keyPressed.key == 'j' ){
-    var audio=  new Audio('sounds/crash.mp3');
+  if( keyPressed.key == 'j'  || keyPressed.key == 'J'){
+    var audio=  new Audio('crash.mp3');
     audio.play();
 
   }
 
-  if(keyPressed.key == 'k'){
-    var audio=  new Audio('sounds/kick-bass.mp3');
+  if(keyPressed.key == 'k' || keyPressed.key == 'K'){
+    var audio=  new Audio('kick-bass.mp3');
     audio.play();
 
   }
 
-  if(keyPressed.key == 'l'){
+  if(keyPressed.key == 'l' || keyPressed.key == 'L'){
 
-    var audio=  new Audio('sounds/snare.mp3');
+    var audio=  new Audio('snare.mp3');
     audio.play();
 
   }
@@ -47,37 +47,37 @@ document.addEventListener('keypress', function( keyPressed){
 
 
 document.querySelectorAll('button')[0].addEventListener('click', function(){
-  var audio=  new Audio('sounds/tom-1.mp3');
+  var audio=  new Audio('tom-1.mp3');
   audio.play();
 
 });
 
 document.querySelectorAll('button')[1].addEventListener('click', function(){
-  var audio=  new Audio('sounds/tom-2.mp3');
+  var audio=  new Audio('tom-2.mp3');
   audio.play();
 });
 
 document.querySelectorAll('button')[2].addEventListener('click', function(){
-  var audio=  new Audio('sounds/tom-3.mp3');
+  var audio=  new Audio('tom-3.mp3');
   audio.play();
 });
 
 document.querySelectorAll('button')[3].addEventListener('click', function(){
-  var audio=  new Audio('sounds/tom-4.mp3');
+  var audio=  new Audio('tom-4.mp3');
   audio.play();
 });
 
 document.querySelectorAll('button')[4].addEventListener('click', function(){
- var audio=  new Audio('sounds/crash.mp3');
+ var audio=  new Audio('crash.mp3');
  audio.play();
 });
 
 document.querySelectorAll('button')[5].addEventListener('click', function(){
-  var audio=  new Audio('sounds/kick-bass.mp3');
+  var audio=  new Audio('kick-bass.mp3');
   audio.play();
 });
 
 document.querySelectorAll('button')[6].addEventListener('click', function(){
-  var audio=  new Audio('sounds/snare.mp3');
+  var audio=  new Audio('snare.mp3');
   audio.play();
 });
